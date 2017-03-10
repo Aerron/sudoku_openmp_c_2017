2342c136afb39fd557cda1cd945d420fdbd11831
SadhuAmarnath <cs15btech11040@iith.ac.in>
2017-03-10 12:21:10 +0530

    Add files via upload

---
+++ 16.sh
@@ -0,0 +1,10 @@
+gcc-6 main.c sudoku.c -O3 -fopenmp -o 1
+
+./1 4 data/16*16/1.txt
+./1 4 data/16*16/2.txt
+./1 4 data/16*16/3.txt
+./1 4 data/16*16/4.txt
+./1 4 data/16*16/5.txt
+./1 4 data/16*16/6.txt
+./1 4 data/16*16/7.txt
+./1 4 data/16*16/8.txt
